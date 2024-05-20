d396a90cda9e8cb6372abfc7d544fae1879747d4
priscilaMayume <prisccila@gmail.com>
2024-05-20 12:19:07 -0300

    rename project

--- pytests/mocks/livraria_mock.py
+++ pytests/mocks/livraria_mock.py
@@ -1,6 +1,6 @@
 def payload_post_livros():
     return {
-        "nome": "Test playrigth-Pri-PY",
+        "nome": "Test playrigth-Pri-PY!",
         "autor": "Pri",
         "dataPublicacao": "2024-05-18",
         "qtdePaginas": 1212
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/priscilahirotsu/pessoal/estudo/projetos/test-plywright-python/venv"
+set -gx VIRTUAL_ENV "/Users/priscilahirotsu/pessoal/estudo/projetos/test-playwright-python/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
