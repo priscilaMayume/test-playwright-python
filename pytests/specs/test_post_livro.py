06bb8a7d3301b18474dcf0e694d7d70efbad3ec8
priscilaMayume <prisccila@gmail.com>
2024-05-20 16:23:17 -0300

    implementação do LOG do Allure Report aos cenarios de teste

--- pytests/specs/test_post_livro.py
+++ pytests/specs/test_post_livro.py
@@ -3,9 +3,11 @@ from pytests.support.hooks import *
 from pytests.mocks.livraria_mock import *
 from pytests.clients.post_livraria_client import PostLivrariaClient
 from pytests.support.api_utils import ApiUtils
+from pytests.schemas.post_registrar_livro_com_sucesso_schema import *
 
 def test_post_livro():
     payload = payload_post_livros()
     ApiUtils.payload_parse(payload)
     response = PostLivrariaClient.post_livros(payload)
     PostLivrariaClient.validate_response(response, 201)
+    ApiUtils.validate_json_schema(response, post_shema)
