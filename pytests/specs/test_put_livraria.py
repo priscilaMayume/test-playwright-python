2cd1c7987acb2b7e748dd9827f73e74617637b07
priscilaMayume <prisccila@gmail.com>
2024-05-21 12:18:52 -0300

    Implementação dos teste de exceção do PUT/Livros

--- pytests/specs/test_put_livraria.py
+++ pytests/specs/test_put_livraria.py
@@ -1,5 +1,6 @@
 from pytests.clients.common import Common
 from pytests.clients.livraria_client import LivrariaClient
+from pytests.exemples.exemples_teste_put_livraria import *
 from pytests.mocks.livraria_mock import *
 from pytests.schemas.put_livro_sucess_schema import *
 from pytests.support.api_utils import ApiUtils
@@ -15,3 +16,49 @@ def test_put_livro_sucess():
     response = client.put_livros(payload, 4)
     Common.validate_response(response, 200)
     ApiUtils.validate_json_schema(response, put_livro_sucess_schema)
+
+
+@pytest.mark.crud_livros
+@pytest.mark.parametrize("payload, code", exemples_put_livraria_invalid_payload)
+def test_put_livros_invalid_payload(payload, code):
+    client = LivrariaClient()
+    client.set_base_url()
+    payload = Common.incorrect_payload(payload)
+    ApiUtils.payload_parse(payload)
+    response = client.put_livros(payload, 4)
+    Common.validate_response(response, code)
+
+
+@pytest.mark.crud_livros
+@pytest.mark.parametrize("field, value, code", exemples_put_livraria_invalid_values)
+def test_put_livros_invalid_values(field, value, code):
+    client = LivrariaClient()
+    client.set_base_url()
+    payload = payload_post_livros()
+    payload = Common.change_fields_payload(payload, field, value)
+    ApiUtils.payload_parse(payload)
+    response = client.put_livros(payload, 4)
+    Common.validate_response(response, code)
+
+
+@pytest.mark.crud_livros
+@pytest.mark.parametrize("field, code", exemples_put_livraria_no_fields)
+def test_put_livros_no_fields(field, code):
+    client = LivrariaClient()
+    client.set_base_url()
+    payload = payload_post_livros()
+    payload = Common.remove_fields_payload(payload, field)
+    ApiUtils.payload_parse(payload)
+    response = client.put_livros(payload, 4)
+    Common.validate_response(response, code)
+
+
+@pytest.mark.crud_livros
+@pytest.mark.parametrize("value, code", exemples_put_id_livraria_invalid_values)
+def test_put_id_livro_invalid(value, code):
+    client = LivrariaClient()
+    client.set_base_url()
+    payload = payload_post_livros()
+    values_change = Common.values_change(value)
+    response = client.put_livros(payload, values_change)
+    Common.validate_response(response, code)
